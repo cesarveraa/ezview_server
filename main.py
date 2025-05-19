4c1d22a1cb70a441e92893ee2d0bf95e60ef47b4
Cesar Vera <soymateovera@gmail.com>
2025-05-18 23:08:39 -0400

    add .gitignore for sensitive files

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # 1) Inicializar Firebase Admin SDK
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
