e9fdb8c0e5dab0d3e0deedf67d2aed32beefe37f
Cesar Vera <soymateovera@gmail.com>
2025-05-18 23:27:19 -0400

    Add missing import for os module in main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from uuid import uuid4
 from datetime import datetime
 import firebase_admin
 from firebase_admin import credentials, firestore
-
+from dotenv import load_dotenv
+import os
 # 1) Carga variables de entorno
 load_dotenv()
 
